2d83fb50caadca94d64d2ba8e1b229ac73454de9
samuelkivi <samuel.kivi98@gmail.com>
2021-03-15 11:59:07 +0200

    3.9 asti tehty

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,25 +1,38 @@
 const mongoose = require('mongoose')
 
 // Replace with the URL of your own database. Do not store the password on GitLab!
-const url = 'mongodb+srv://web-and-mobile:mymongodbdatabase@cluster0.lx7sz.mongodb.net/myFirstDatabase?'
+const url = 'mongodb+srv://web-and-mobile:secret@cluster0.lx7sz.mongodb.net/myFirstDatabase?'
+
+var args = process.argv.slice(2);
 
 mongoose.connect(url)
 
-const Reminder = mongoose.model('Reminder', {
+const reminderSchema = mongoose.model('Reminder', {
     content: String,
     date: Date,
     important: Boolean
 })
 
-const reminder = new Reminder({
-    content: 'HTML on helppoa',
-    date: new Date(),
-    important: true
-})
-
-reminder
-    .save()
-    .then(response => {
-        console.log('reminder saved!')
-        mongoose.connection.close()
+if (args.length === 0) {
+    reminderSchema
+        .find({})
+        .then(result => {
+            result.forEach(reminder => {
+                console.log(reminder)
+            })
+            mongoose.connection.close()
+        })
+}
+else {
+    const reminder = new reminderSchema({
+        content: args[0],
+        date: args[1],
+        important: false
     })
+    reminder
+        .save()
+        .then(response => {
+            console.log(reminder + ' reminder saved!')
+            mongoose.connection.close()
+        })
+}
